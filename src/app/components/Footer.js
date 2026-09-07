f421d5ee3750cbefa9d04eff5489051f621dee4e
Andreza <campbell.andreza@gmail.com>
2026-09-07 13:39:26 -0300

    update footer year automatically

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-black">
       <section className="mx-auto w-full  max-w-screen-xl">
         <section className="flex h-[50px] pt-4 justify-center text-center">
-          <p>SpaceY - All rights reserved - 2004</p>
+          <p>SpaceY - All rights reserved - 2024 - {currentYear}</p>
         </section>
       </section>
     </footer>
